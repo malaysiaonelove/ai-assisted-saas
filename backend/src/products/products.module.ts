bf48669d5cdef56c114f73f3e9ba169dc416e97e
malaysiaonelove <malaysiaonelove@gmail.com>
2026-08-16 11:46:36 -0700

    Add UsageModule to ProductsModule imports

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from '../common/common.module';
+import { UsageModule } from '../usage/usage.module';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, UsageModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
