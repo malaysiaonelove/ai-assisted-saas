d626708df6962bfdb47e5b3d3af97ff329ed3374
malaysiaonelove <malaysiaonelove@gmail.com>
2026-08-16 11:52:23 -0700

    Add UsageModule to SalesModule imports

--- backend/src/sales/sales.module.ts
+++ backend/src/sales/sales.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from '../common/common.module';
+import { UsageModule } from '../usage/usage.module';
 import { SalesController } from './sales.controller';
 import { SalesService } from './sales.service';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, UsageModule],
   controllers: [SalesController],
   providers: [SalesService],
 })
